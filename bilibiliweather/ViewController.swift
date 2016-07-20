9430eb04f938056ffe53036c81e9e4283bf41631
cdroad <cdroad@Hotmail.com>
2016-07-20 11:31:15 +0800

    update

---
+++ bilibiliweather/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  bilibiliweather
+//
+//  Created by 林昱帆 on 7/18/16.
+//  Copyright © 2016 林昱帆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBAction func setCityButtonTapped(sender: AnyObject) {
+        let alert = UIAlertController(title: "Set City",message: nil,preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Cancel",style: .Cancel,handler: nil))
+        alert.addAction(UIAlertAction(title: "ok",style: .Default,handler: {(action) in
+            if let tf =  alert.textFields?.first{
+                if let cityName = tf.text{
+                    self.getWeatherData(cityName)
+                }
+            }
+            
+        }))
+        alert.addTextFieldWithConfigurationHandler{(tf) in
+            tf.placeholder = "enther you city  name here"
+        }
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+
+    
+    private func getWeatherData(city:String){
+    //todo
+        //mypath d6127322479ba3819ef01fe42f26d526
+    
+        let cityName = city.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet())
+        let path = "http://api.openweathermap.org/data/2.5/weather?q=\(cityName)London&appid=d6127322479ba3819ef01fe42f26d526"
+        let url = NSURL(string:path)
+        let request = NSURLRequest(URL:url!)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request){(data,response,error)in
+            if error == nil{
+                print("get data")
+                
+            }else{
+            print(error)
+            }
+            
+        }
+    task.resume()
+        
+        
+        
+    }
+    
+    private func updateUI(city:String,temp:Double,description:String){
+        cityLabel.text = city
+        tempLabel.text = String(temp) + "c"
+        descriptionLabel.text = description
+        
+    
+    
+    }
+
+}
+
---
+++ bilibiliweatherTests/bilibiliweatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  bilibiliweatherTests.swift
+//  bilibiliweatherTests
+//
+//  Created by 林昱帆 on 7/18/16.
+//  Copyright © 2016 林昱帆. All rights reserved.
+//
+
+import XCTest
+@testable import bilibiliweather
+
+class bilibiliweatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
