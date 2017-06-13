20b8d644b7334bf4d9d83ebb95941abdf99442be
LiulietLee <lee.liuliet@hotmail.com>
2017-06-13 18:12:32 +0800

    update

--- bilibiliweather/ViewController.swift
+++ bilibiliweather/ViewController.swift
@@ -13,63 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    @IBAction func setCityButtonTapped(sender: AnyObject) {
-        let alert = UIAlertController(title: "Set City",message: nil,preferredStyle: .Alert)
-        alert.addAction(UIAlertAction(title: "Cancel",style: .Cancel,handler: nil))
-        alert.addAction(UIAlertAction(title: "ok",style: .Default,handler: {(action) in
-            if let tf =  alert.textFields?.first{
-                if let cityName = tf.text{
-                    self.getWeatherData(cityName)
-                }
-            }
-            
-        }))
-        alert.addTextFieldWithConfigurationHandler{(tf) in
-            tf.placeholder = "enter you city  name here"
-        }
-        self.presentViewController(alert, animated: true, completion: nil)
-        
-    }
-
     
-    private func getWeatherData(city:String){
-    //todo
-        //mypath d6127322479ba3819ef01fe42f26d526
-    
-        let cityName = city.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet())
-        let path = "http://api.openweathermap.org/data/2.5/weather?q=\(cityName!)&appid=d6127322479ba3819ef01fe42f26d526"
-        let url = NSURL(string:path)
-        let request = NSURLRequest(URL:url!)
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request){(data,response,error)in
-            if error == nil{
-                let json = JSON(data:data!)
-                let name = json["name"].string!
-                let temp = json["main"]["temp"].double! - 273.15
-                let description = json["weather"][0]["description"].string!
-                
-                dispatch_sync(dispatch_get_main_queue(), {
-                    self.updateUI(name, temp: temp, description: description)
-                })
-                print("done!")
-            }else{
-            print(error)
-            }
+    fileprivate func getWeatherData(_ city:String){
+        let path = "http://api.openweathermap.org/data/2.5/weather?q=london&appid=d6127322479ba3819ef01fe42f26d526"
+        let url = URL(string: path)
+        let request = URLRequest(url: url!)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
             
-        }
-    task.resume()
-        
-        
-        
+            
+            
+        })
+        task.resume()
     }
     
-    private func updateUI(city:String,temp:Double,description:String){
+    fileprivate func updateUI(_ city:String,temp:String,description:String){
         cityLabel.text = city
-        tempLabel.text = String(temp) + "c"
+        tempLabel.text = temp + "℃"
         descriptionLabel.text = description
-        
-    
-    
     }
 
 }
